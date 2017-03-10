29331e37ff3bf309cf279121eb5463c807a37381
NathalieStiefsohn <nathalie.stiefsohn@hotmail.com>
2017-03-10 14:24:26 +0100

    only array missing

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 $(init);
 
-    function init() {
-        $('.draggable').draggable({
-        	snap: ".droppable",
-        	cursor: "move", cursorAt: { top: 50, left: 50 },
-        	revert: "invalid",
-        	stack: ".draggable"
-        });
+function init() {
+    $('.draggable').draggable({
+        snap: ".droppable",
+        cursor: "move",
+        cursorAt: { top: 50, left: 50 },
+        revert: "invalid",
+        stack: ".draggable",
 
-        // for (i=0;i<$(".droppable").length;i++){
-        // $(".droppable").eq(i).droppable({accept: $(".draggable")[i]});
-        // }//end for loop
-    } //end function
+    });
+
+    for (i = 0; i < $(".droppable").length; i++) {
+        // var draggable = $(".draggable")[$(this).index()];
+        $(".droppable").eq(i).droppable({
+            accept: $(".draggable")[i],
+            drop: function(event, ui) {
+                    $(".draggable").eq($(this).index()).addClass("dropped");
+                    console.log($(".droppable").length);
+                    console.log($(".dropped").length);
+                    console.log($(this).index())
+                        // console.log(draggable);
+                }
+                // drop: function() { $('.draggable').eq(i).addClass("dropped") }
+        })
+    } //end for loop
+} //end function
+
+
+function random_order () {
+var cards = $(".draggable");
+for(var i = 0; i < cards.length; i++){
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+    cards.eq(target).before(cards.eq(target2));
+}
+}
+random_order();
